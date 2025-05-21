1451f33bc10f097cfa78125ec87699f11a61d1fe
Murad Aliyev <skyfarmx@gmail.com>
2025-05-22 00:08:20 +0300

    Update models.py

--- user_registration/models.py
+++ user_registration/models.py
@@ -2,33 +2,29 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Users(models.Model):
-
+class Kullanicilar(models.Model):
+    """
+    Kullanıcı profil bilgilerini saklayan model.
+    Django'nun varsayılan User modeline ek bilgiler ekler.
+    """
     id = models.AutoField(auto_created=True, primary_key=True, serialize=False)
     kat_id = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=250, verbose_name='Adı')
     father_name = models.CharField(max_length=250, verbose_name='Ata adı')
     last_name = models.CharField(max_length=250, verbose_name='Soyadı')
     city = models.CharField(max_length=250, verbose_name='Ülke ve ya Şehir')
-    picture = models.ImageField(upload_to='assets/images',blank=True, null=True,verbose_name='image')
-    phone = models.CharField(max_length=250, verbose_name='phone')
-    departments = models.CharField(max_length=250, verbose_name='departments')
-    gender = models.CharField(max_length=250, verbose_name='cinsi')
-    bio = models.CharField(max_length=2500, verbose_name='bio')
+    picture = models.ImageField(upload_to='assets/images', blank=True, null=True, verbose_name='Resim')
+    phone = models.CharField(max_length=250, verbose_name='Telefon')
+    departments = models.CharField(max_length=250, verbose_name='Departmanlar')
+    gender = models.CharField(max_length=250, verbose_name='Cinsiyet')
+    bio = models.CharField(max_length=2500, verbose_name='Biyografi')
     birthday = models.DateField(verbose_name='Doğum günü')
-    address = models.CharField(max_length=2500, verbose_name='Ünvan')   
-    website = models.URLField(max_length=250,default=None, verbose_name='Veb sayt')   
+    address = models.CharField(max_length=2500, verbose_name='Adres')   
+    website = models.URLField(max_length=250, default=None, verbose_name='Web sitesi')   
 
     def __str__(self):
         return self.first_name
 
-
-
-
-
-#/var/www/data/d220046e-e471-4a82-832a-4af2509a24f5/images
-
-
-
-
-
+    class Meta:
+        verbose_name = 'Kullanıcı Profili'
+        verbose_name_plural = 'Kullanıcı Profilleri'
