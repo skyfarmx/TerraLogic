3af6c22d47a02eee4cd305dea1c5e46c0a781528
Murad Aliyev <skyfarmx@gmail.com>
2025-05-22 00:07:50 +0300

    Update forms.py

--- user_registration/forms.py
+++ user_registration/forms.py
@@ -1,26 +1,25 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm  
-from user_registration.models import Users
+from user_registration.models import Kullanicilar
 from django.contrib import admin
 
 
-
-class UserForm(UserCreationForm):
+class KullaniciFormu(UserCreationForm):
+    """
+    Kullanıcı oluşturma formu, e-posta alanı eklenmiş hali.
+    """
     email = forms.EmailField(max_length=200) 
     USERNAME_FIELD = 'email' 
 
 
-class UsersForm(forms.ModelForm):
-	class Meta:
-		model = Users
-		fields = '__all__'
+class KullanicilarFormu(forms.ModelForm):
+    """
+    Kullanıcı profil bilgileri için form.
+    """
+    class Meta:
+        model = Kullanicilar
+        fields = '__all__'
 
 
-admin.site.register(Users)
-
-
-
-
-
-
-
+# Admin panelinde Kullanicilar modelini kaydet
+admin.site.register(Kullanicilar)
